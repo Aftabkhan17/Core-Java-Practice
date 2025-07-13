f0dc2754b2ed9455828d530753060fec9f4c9be7
Aftab <aftabkhan.m.m.k@gmail.com>
2025-07-13 11:36:59 +0530

    Addition of the Two Variables

--- Basics/VariablesDemo.java
+++ Basics/VariablesDemo.java
@@ -1,3 +1,20 @@
+// Example of the VariablesDemo.java
+
+package Basics;
 public class VariablesDemo {
-    
+    public static void main(String[] args) {
+
+
+        // int num = 5;
+        
+        // System.out.println(num);
+
+        int num1 = 5;
+        int num2 = 6;
+        int result = num1 + num2;
+        System.out.println(result);
+
+ 
+
+    }  
 }
