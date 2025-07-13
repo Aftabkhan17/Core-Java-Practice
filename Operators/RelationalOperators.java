d158924098f8020c83979eb3fa63941b6864088a
Aftab <aftabkhan.m.m.k@gmail.com>
2025-07-13 14:58:57 +0530

    Performed the Relational Operators

--- Operators/RelationalOperators.java
+++ Operators/RelationalOperators.java
@@ -1,5 +1,49 @@
-
+package Operators;
 
 public class RelationalOperators {
+    public static void main(String[] args) {
+        
+        // int x = 6;
+        // int y = 5;
+
+        // boolean result = x > y;
+        // System.out.println(result);    //Output: true
+
+        // int x = 6;
+        // int y = 5;
+
+        // boolean result = x < y;
+        // System.out.println(result); //Output: false
+
+        // int x = 6;
+        // int y = 6;
+
+        // boolean result = x > y;
+        // System.out.println(result);  //Output: false
+
+        // int x = 6;
+        // int y = 6;
+
+        // boolean result = x >= y;
+        // System.out.println(result);  //Output: true
+
+        // int x = 6;
+        // int y = 6;
+
+        // boolean result = x != y;
+        // System.out.println(result);  //Output: false
+
+
+        // int x = 6;
+        // int y = 6;
+        // boolean result = x == y;   //this operator is used to check they are similar
+        // System.out.println(result); //Output: True
+
+        
+        int x = 6;
+        int y = 7;
+        boolean result = x == y;   //this operator is used to check they are similar
+        System.out.println(result); //Output: false
+    }
     
 }
