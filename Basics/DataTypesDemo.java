bea209268d422bcd287236d4dcafa37a415e2c79
Aftab <aftabkhan.m.m.k@gmail.com>
2025-07-13 12:28:29 +0530

    Seen datatypes  and a small exercise for variables and datatypes

--- Basics/DataTypesDemo.java
+++ Basics/DataTypesDemo.java
@@ -1,3 +1,41 @@
+
+package Basics;
+
 public class DataTypesDemo {
+    public static void main(String[] args) {
+        
+
+        // int num1 = 9;
+        // byte by = 127;
+
+        // short sh = 558;
+        // long l = 5858l;
+
+        // float f = 5.8f;
+        // double d = 5.82584;
+
+        // char c = 'k';
+
+        // boolean b = true;
+
+
+
+        // // float marks = 6.5;
+        // System.out.println();
+
+        // Exercise for Variables & data types 
+       // write code here
+       
+       int age = 25;
+       double height = 5.9;
+       String name = "Alex";
+       
+   System.out.println("Name: " + name + ", Age: " + age + ", Height: " + height);
+
+
+
+
+    }
+
     
 }
