4030248a34f6cd00f5d02887930761c867949627
Aftab <aftabkhan.m.m.k@gmail.com>
2025-07-15 13:28:04 +0530

    performed the switchCase & Done a simple Exercise

--- ControlFlow/SwitchCaseDemo.java
+++ ControlFlow/SwitchCaseDemo.java
@@ -5,49 +5,49 @@ public class SwitchCaseDemo {
         
 
         //simple if-else statement 
-       // int n = 1;
-        // if(n==1)
-        // System.out.println("Monday");
-        // else if(n==2)
-        // System.out.println("Tuesday");
-        // else if(n==3)
-        // System.out.println("Wednesday");
-        // else if(n==4)
-        // System.out.println("Thursday");
-        // else if(n==5)
-        // System.out.println("Friday");
-        // else if(n==6)
-        // System.out.println("Saturday");
-        // else
-        // System.out.println("Sunday");
+       int n = 1;
+        if(n==1)
+        System.out.println("Monday");
+        else if(n==2)
+        System.out.println("Tuesday");
+        else if(n==3)
+        System.out.println("Wednesday");
+        else if(n==4)
+        System.out.println("Thursday");
+        else if(n==5)
+        System.out.println("Friday");
+        else if(n==6)
+        System.out.println("Saturday");
+        else
+        System.out.println("Sunday");
 
         //Switch Case
-    //    int n = 8;
+       int n = 8;
 
-    //    switch(n){
-    //     case 1:
-    //     System.out.println("Monday");
-    //     break;
-    //     case 2:
-    //     System.out.println("Tuesday");
-    //     break;
-    //     case 3:
-    //     System.out.println("Wednesday");
-    //     break;
-    //     case 4:
-    //     System.out.println("Thursday");
-    //     break;
-    //     case 5:
-    //     System.out.println("Friday");
-    //     break;
-    //     case 6:
-    //     System.out.println("Saturday");
-    //     break;
-    //     case 7:
-    //     System.out.println("Sunday");
-    //     break;
-    //     default:
-    //     System.out.println("Enter the valid number");
+       switch(n){
+        case 1:
+        System.out.println("Monday");
+        break;
+        case 2:
+        System.out.println("Tuesday");
+        break;
+        case 3:
+        System.out.println("Wednesday");
+        break;
+        case 4:
+        System.out.println("Thursday");
+        break;
+        case 5:
+        System.out.println("Friday");
+        break;
+        case 6:
+        System.out.println("Saturday");
+        break;
+        case 7:
+        System.out.println("Sunday");
+        break;
+        default:
+        System.out.println("Enter the valid number");
         
         //Java Conditionals: Evaluate Student Result with If-Else and Grading Logic
         int score = 99;
